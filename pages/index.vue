857f2f498aa1b5949271d61fab75973d8c1cc90b
Solomon Eseme <kaperskyguru@gmail.com>
2024-05-10 16:17:54 +0100

    fix: add advert

--- pages/index.vue
+++ pages/index.vue
@@ -145,9 +145,15 @@
           </div>
         </section>
 
-        <section id="vert">
+        <!-- <div class="container relative mx-auto md:w-3/4 w-full">
+          <div class="flex absolute right-0 -top-5 justify-end">
+            <p class="bg-black py-2 px-3 text-white rounded-full"></p>
+          </div>
+        </div> -->
+
+        <section id="vert" class="py-2 md:mt-3">
           <div
-            class="flex md:flex-row flex-col group bg-gradient-to-r from-purple-600 to-blue-600 text-white py-2 pl-2 md:justify-between md:rounded-lg rounded justify-start items-center gap-3 container mx-auto md:w-3/4 w-full px-2 md:mt-3 border-t md:border border-solid border-gray-300 md:pr-20 pr-5"
+            class="flex md:flex-row flex-col group bg-gradient-to-r from-purple-600 to-blue-600 text-white pl-2 md:justify-between md:rounded-lg rounded justify-start items-center gap-3 container mx-auto md:w-3/4 w-full px-2 border-t md:border border-solid border-gray-300 md:pr-20 pr-5"
           >
             <div class="flex md:flex-row flex-col items-center gap-3 w-full">
               <div><Avatar size="normal" name="Mastering Backend" /></div>
@@ -177,6 +183,58 @@
               </a>
             </div>
           </div>
+        </section>
+
+        <section id="vert" class="py-2 md:mt-3">
+          <a href="https://tally.so/r/wblgQ6" target="_blank">
+            <div
+              class="flex md:flex-row flex-col group bg-white pl-2 md:justify-between md:rounded-lg rounded justify-start items-center gap-3 container mx-auto md:w-3/4 w-full px-2 border-t md:border border-dashed border-gray-400 md:pr-20 pr-5"
+            >
+              <div class="flex md:flex-row flex-col items-center gap-3 w-full">
+                <div><Avatar size="normal" src="/yourlogo.png" /></div>
+                <div class="py-4">
+                  <div>
+                    <h2 class="text-2xl text-gray-700">
+                      Your brand seen by (<a
+                        target="_blank"
+                        class="text-red-600 underline"
+                        href="https://plausible.io/getbackendjobs.com?period=30d"
+                        >{{
+                          new Intl.NumberFormat("en", {
+                            notation: "compact",
+                          }).format(pageviews)
+                        }}/mo</a
+                      >) people
+                    </h2>
+                  </div>
+                  <div class="py-1 flex">
+                    <p class="text-lg text-gray-400">
+                      Imagine your business here with over (<a
+                        target="_blank"
+                        href="https://plausible.io/getbackendjobs.com?period=30d"
+                        >{{
+                          new Intl.NumberFormat("en", {
+                            notation: "compact",
+                          }).format(pageviews)
+                        }}/mo </a
+                      >) pageviews
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div
+                class="relative flex items-center md:w-2/6 w-full justify-center"
+                style="align-content: baseline"
+              >
+                <button
+                  disabled
+                  class="border-dashed text-gray-400 border border-gray-400 px-5 rounded-lg py-1"
+                >
+                  Your call to action
+                </button>
+              </div>
+            </div>
+          </a>
         </section>
 
         <section>
@@ -234,6 +292,7 @@ import { computed, onMounted } from "vue";
 const el = ref(null);
 const loading = ref(false);
 const filters = ref({});
+const pageviews = ref(0);
 const jobs = ref([]);
 const pinJobs = ref([]);
 const isLast = ref(false);
@@ -406,6 +465,30 @@ const unPinJobs = computed(() => {
       return job;
   });
 });
+
+async function getPlausableStat() {
+  try {
+    const res = await useFetch(
+      "https://plausible.io/api/v1/stats/aggregate?site_id=getbackendjobs.com&period=30d&metrics=pageviews",
+      {
+        onRequest({ request, options }) {
+          // Set the request headers
+          options.headers = options?.headers || {};
+          options.headers.authorization =
+            "Bearer wwqzv__3pglKkpxVP1onp6XEWV4wtp0U4EczOF-u8xUBTabByzArPi7qK6gOzHcW";
+        },
+      }
+    );
+    const results = res.data.value?.results;
+
+    pageviews.value = results?.pageviews?.value ?? 0;
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+}
+
+await getPlausableStat();
 </script>
 
 
