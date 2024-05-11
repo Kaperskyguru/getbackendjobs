e71488da8149bf00879e6b96bc44927640f5b669
Solomon Eseme <kaperskyguru@gmail.com>
2024-05-11 17:39:06 +0100

    fix: update placeAdd

--- components/PlaceAds.vue
+++ components/PlaceAds.vue
@@ -6,50 +6,52 @@
       @click.prevent="adClickEvent('pages_ad')"
       href="https://tally.so/r/wblgQ6"
       target="_blank"
-      class="flex md:flex-row flex-col items-center gap-3 w-full"
+      class="flex justify-between w-full"
     >
-      <div><Avatar size="normal" src="/yourlogo.png" /></div>
-      <div class="py-4">
-        <div>
-          <h2 class="text-2xl text-gray-700">
-            Your brand seen by (<a
-              target="_blank"
-              class="text-red-600 underline"
-              href="https://plausible.io/getbackendjobs.com?period=30d"
-              >{{
-                new Intl.NumberFormat("en", {
-                  notation: "compact",
-                }).format(pageviews)
-              }}/mo</a
-            >) people
-          </h2>
-        </div>
-        <div class="py-1 flex">
-          <p class="text-lg text-gray-400">
-            Imagine your business here with over (<a
-              target="_blank"
-              href="https://plausible.io/getbackendjobs.com?period=30d"
-              >{{
-                new Intl.NumberFormat("en", {
-                  notation: "compact",
-                }).format(pageviews)
-              }}/mo </a
-            >) pageviews
-          </p>
+      <div class="flex md:flex-row flex-col items-center gap-3 w-full">
+        <div><Avatar size="normal" src="/yourlogo.png" /></div>
+        <div class="py-4">
+          <div>
+            <h2 class="text-2xl text-gray-700">
+              Your brand seen by (<a
+                target="_blank"
+                class="text-red-600 underline"
+                href="https://plausible.io/getbackendjobs.com?period=30d"
+                >{{
+                  new Intl.NumberFormat("en", {
+                    notation: "compact",
+                  }).format(pageviews)
+                }}/mo</a
+              >) people
+            </h2>
+          </div>
+          <div class="py-1 flex">
+            <p class="text-lg text-gray-400">
+              Imagine your business here with over (<a
+                target="_blank"
+                href="https://plausible.io/getbackendjobs.com?period=30d"
+                >{{
+                  new Intl.NumberFormat("en", {
+                    notation: "compact",
+                  }).format(pageviews)
+                }}/mo </a
+              >) pageviews
+            </p>
+          </div>
         </div>
       </div>
-    </a>
-    <div
-      class="relative flex items-center md:w-2/6 w-full justify-center"
-      style="align-content: baseline"
-    >
-      <button
-        disabled
-        class="border-dashed text-gray-400 border border-gray-400 px-5 rounded-lg py-1"
+      <div
+        class="relative flex items-center md:w-2/6 w-full justify-center"
+        style="align-content: baseline"
       >
-        Your call to action
-      </button>
-    </div>
+        <button
+          disabled
+          class="border-dashed text-gray-400 border border-gray-400 px-5 rounded-lg py-1"
+        >
+          Your call to action
+        </button>
+      </div>
+    </a>
   </div>
 </template>
 
