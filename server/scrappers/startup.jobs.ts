67c32ba8858dd940dcf6c5ab733ca0d23d362ec5
Solomon Eseme <kaperskyguru@gmail.com>
2024-07-22 12:48:21 +0100

    fix: update path

--- server/scrappers/startup.jobs.ts
+++ server/scrappers/startup.jobs.ts
@@ -1,6 +1,6 @@
 import { dbJobResolver } from "../helpers";
 import { Builder, By } from "selenium-webdriver";
-import chrome from "selenium-webdriver/chrome";
+import chrome from "selenium-webdriver/chrome.js";
 import { addJobs, addJobsHelper } from "../api/services";
 const jobUrl = `https://startup.jobs/?q=backend`;
 
