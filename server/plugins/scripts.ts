5449383648879740994afb2d7eb59cc55e36722e
Solomon Eseme <kaperskyguru@gmail.com>
2024-10-08 13:31:40 +0100

    fix: add clarity

--- server/plugins/scripts.ts
+++ server/plugins/scripts.ts
@@ -24,6 +24,14 @@ export default defineNitroPlugin((nitroApp: any) => {
   <script src="https://cdn.usefathom.com/script.js" data-site="HTSWREGQ" defer></script>
 <!-- / Fathom -->
 
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "off2nbtvix");
+</script>
+
     `);
   });
 });
