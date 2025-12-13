17893b52f6c58b79139627986827d7ac832dc6b2
Solomon Eseme <kaperskyguru@gmail.com>
2025-12-13 09:21:32 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -6,5 +6,6 @@
         <NuxtPage />
       </div>
     </NuxtLayout>
+    <script src="https://v0-announcement-banner-saa-s.vercel.app/api/embed/fc8c7605b9dac4df30a30a33e60d6410.js" async></script>
   </div>
 </template>
